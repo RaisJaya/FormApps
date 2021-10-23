fa0b74ff7807c550a284b6c758bc257ece2cbbdc
Raisjaya <ahmad.rdj2@gmail.com>
2021-10-23 20:40:45 +0700

    Menambahkan DashboardActivity & QuizActivity

--- app/src/main/java/com/raisjayadevelop/formapps/MainActivity.java
+++ app/src/main/java/com/raisjayadevelop/formapps/MainActivity.java
@@ -2,6 +2,7 @@ package com.raisjayadevelop.formapps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,28 +11,27 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    final public static String EXTRA_NAME = "extra_nama";
+    final public static String EXTRA_EMAIL = "extra_email";
+    final public static String EXTRA_PHONE = "extra_phone";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText inputName = (EditText) findViewById((R.id.idInputNama));
-        EditText inputEmail = (EditText) findViewById((R.id.idInputEmail));
-        EditText inputPhone = (EditText) findViewById((R.id.idInputPhone));
-        EditText inputPassword = (EditText) findViewById((R.id.eidInputPwd));
+        EditText editName = (EditText) findViewById((R.id.idInputNama));
+        EditText editEmail = (EditText) findViewById((R.id.idInputEmail));
+        EditText editPhone = (EditText) findViewById((R.id.idInputPhone));
+        EditText editPassword = (EditText) findViewById((R.id.eidInputPwd));
 
         Button btnSave = (Button) findViewById(R.id.idButtonSave);
-        btnSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Kode aksi dari eksekusi klik button
-
-                Log.d(getPackageName()+" Nama", inputName.getText().toString());
-                Log.d(getPackageName()+" Email", inputEmail.getText().toString());
-                Log.d(getPackageName()+" Phone", inputPhone.getText().toString());
-                Log.d(getPackageName()+" Password", inputPassword.getText().toString());
-
-            }
+        btnSave.setOnClickListener(v -> {
+            Intent loginIntent = new Intent(MainActivity.this, DashboardActivity.class);
+            loginIntent.putExtra(EXTRA_NAME, editName, getText().toString());
+            loginIntent.putExtra(EXTRA_EMAIL, editEmail, getText().toString());
+            loginIntent.putExtra(EXTRA_PHONE, editPhone, getText().toString());
+            startActivity(loginIntent);
         });
 
     }
